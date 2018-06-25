c2a5fb7ee20f1c241d63b38f274a6598c8f99267
fernando <lujano14.93@gmail.com>
2018-06-25 00:03:17 -0500

    estilos en las alertas

--- resources/views/elements/messages.blade.php
+++ resources/views/elements/messages.blade.php
@@ -2,17 +2,21 @@
 @if (session('alert'))
 
 	@php
-		$attr_clss =  Auth::user()->isAdmin() ? "alert-dismissible fade show" : ""; 	
+		$user= Auth::user();
+		$attr_clss =  $user &&$user->isAdmin() ? "alert-dismissible fade show" : ""; 	
 		$attr_clss_status  = session('alert.status') === 'ok' ? 'alert-success' :  
     		(   session('alert.status')==='warning' ? 'alert-warning'   :'alert-danger') ;
 
     	$floatStyleClss = isset($floatMsg) &&  $floatMsg  ? 'home-float-alert' : '';
 	@endphp
-
-    <div class="alert  {{  $attr_clss_status }}  {{ $attr_clss }} {{ $floatStyleClss }}   " role="alert" >
-        {{ session('alert.message') }}
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			    <span aria-hidden="true">&times;</span>
-			  </button>
-    </div>
+	<div class="group-alert {{ $floatStyleClss }}  ">
+		 <div class="alert  {{  $attr_clss_status }}  {{ $attr_clss }}  " role="alert" >
+	        {{ session('alert.message') }}
+	          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				    <span aria-hidden="true">&times;</span>
+				  </button>
+	    </div>
+		
+	</div>
+   
 @endif
