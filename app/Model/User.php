6860e07fb1aedf260ba86793fce0f1aa8e2ef2fe
fernando <lujano14.93@gmail.com>
2019-02-22 21:02:57 -0600

    pruebas

--- app/Model/User.php
+++ app/Model/User.php
@@ -66,7 +66,7 @@ class User extends Authenticatable
 
 
      public function isAdmin(){
-       return $this->id_role ===  User::ROLE_ADMIN;
+       return $this->id_role ==  User::ROLE_ADMIN;
 
      }
 
